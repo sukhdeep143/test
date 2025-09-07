66ab367333d464b11634984ff2f25e378cb76d4c
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-09-07 23:43:39 +0530

    update

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,10 +1,42 @@
+import { useState } from "react"
 
 
 
 export default function Register(){
+    const [form , setFrom] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+    const [message, setMessage] = useState("");
     return(
-        <section>
-            
-        </section>
+        <form
+        onSubmit={HandelSubmit}
+
+
+        >
+            <input type="text"
+            name="name"
+            placeholder="Full name"
+            value={form.name}
+            onChange={handleChange}
+            required
+             
+             />
+             <input type="email"
+             name="email"
+             placeholder="Enter email"
+             value={form.email}
+             onChange={handleChange}
+             required />
+             
+             <input type="password"
+             name="password"
+             placeholder="Enter pssword"
+             value={form.password}
+             onChange={handleChange}
+             required
+              />
+        </form>
     )
 }
